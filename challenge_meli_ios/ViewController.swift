318d408b5517de93756f247383f21b6571eac944
Julian Salazar <hjsalazar9@gmail.com>
2025-02-23 13:26:00 -0500

    Initialize project structure with folders

---
+++ challenge_meli_ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  challenge_meli_ios
+//
+//  Created by Mac on 23/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
